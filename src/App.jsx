bcf2800f2f77600136609fdc14b17b8713f8d25d
Psquare31 <162909668+Psquare31@users.noreply.github.com>
2024-12-17 13:21:52 +0530

    first commit navbar and sidebar layout

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//import './App.css'
+import Bars from './components/Bars'
+
+function App() {
+
+  return (
+    <>
+    <Bars></Bars>
+    </>
+  )
+}
+
+export default App
